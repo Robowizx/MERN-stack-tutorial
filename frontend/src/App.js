339bc73edfe9735fd20c31f5ae369276781b3a51
Robowizx <omkariyer99@gmail.com>
2019-09-19 15:16:01 +0530

    background change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import 'antd/dist/antd.css';
 import CreateForm from './Components/create';
 import Read from './Components/read';
 import DeleteForm from './Components/delete';
+import back from './MERN.jpg';
 
 const { Content } = Layout;
 
 
-
 class App extends Component {
-  state = { option: "home" };
+  state = { option: "home"};          
 
   handleClick = e =>{
      this.setState({option: e.key});
@@ -18,7 +18,7 @@ class App extends Component {
   tabSelect = ()=>{
      switch(this.state.option)
      {
-        case "home": return <div><img style={{margin:'200px 530px 0px'}} src='mongodb.png' height='159px' width='585px'></img><h1 style={{margin: '50px 625px 460px',fontSize:'50px'}}>Database interface</h1></div>;
+        case "home": return <div style={{backgroundImage:`url(${back})`,backgroundSize:"1680px 865px",height:"800px"}}></div>;
         case "create": return <CreateForm/>;
         case "read": return <Read/>;
         case "delete": return <DeleteForm/>;
@@ -36,7 +36,7 @@ class App extends Component {
                      <Menu.Item key="update">Update</Menu.Item>
                      <Menu.Item key="delete">Delete</Menu.Item>
                   </Menu>
-            <Content style={{background:'#ffff'}}>
+            <Content style={{background:"#ffff"}}>
                {this.tabSelect()}
             </Content>
          </Layout>
